d4109bc8b4e3f07516490b58a458d7e0bd53ef06
Inch129 <so0utt@gmail.com>
2015-09-09 01:48:59 +0400

    no comment

---
+++ app/src/main/java/com/asgard/powerthesaurus/MainActivity.java
@@ -0,0 +1,67 @@
+package com.asgard.powerthesaurus;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView web;
+
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
+            web.goBack();
+            return true;
+        } else {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Exit!")
+                    .setMessage("Are you sure you want to close?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+            // If it wasn't the Back key or there's no web page history, bubble up to the default
+            // system behavior (probably exit the activity)
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web = (WebView) findViewById(R.id.webView);
+
+        web.setWebViewClient(new HelloWebViewClient());
+
+        web.getSettings().setJavaScriptEnabled(true);
+
+        web.loadUrl("https://powerthesaurus.org/");
+
+    }
+
+
+}
